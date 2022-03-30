7da91164d1939fab89556dcbb589558ba900c420
chlwodnjs <wodnjs5171@gmail.com>
2022-03-30 21:27:38 +0900

    inf

--- de_python/yield.py
+++ de_python/yield.py
@@ -15,8 +15,21 @@ def yield_abc():
         yield ch
 
 
+def yield_infinite_abc():
+    while True:
+        yield "A"
+        yield "B"
+        yield "C"
+
+# def yield_abc():
+#   yield from ["A", "B", "C"]
+
+
 for ch in return_abc():
     print(ch)
 
 for ch in yield_abc():
+    print(ch)
+
+for ch in yield_infinite_abc():
     print(ch)
